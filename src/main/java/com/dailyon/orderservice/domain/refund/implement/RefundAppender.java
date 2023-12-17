b471918ee1ffaba60a7fc7f5f38f2f3d843bcffb
JunYong Sun <ssjy4974@naver.com>
2023-12-18 05:24:53 +0900

    O2F-681 [TEST] 주문 취소 api 테스트

--- src/main/java/com/dailyon/orderservice/domain/refund/implement/RefundAppender.java
+++ src/main/java/com/dailyon/orderservice/domain/refund/implement/RefundAppender.java
@@ -1,14 +1,10 @@
 package com.dailyon.orderservice.domain.refund.implement;
 
-import com.dailyon.orderservice.domain.order.entity.Order;
 import com.dailyon.orderservice.domain.order.entity.OrderDetail;
 import com.dailyon.orderservice.domain.refund.entity.Refund;
 import com.dailyon.orderservice.domain.refund.repository.RefundRepository;
-import com.dailyon.orderservice.domain.refund.utils.RefundCalculator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import static com.dailyon.orderservice.domain.refund.utils.RefundCalculator.findRefundCalculator;
 
 @RequiredArgsConstructor
 @Component
@@ -16,7 +12,6 @@ public class RefundAppender {
   private final RefundRepository refundRepository;
 
   public Refund append(OrderDetail orderDetail, int refundPoints, int refundableAmount) {
-
     Refund refund =
         Refund.builder()
             .order(orderDetail.getOrder())
