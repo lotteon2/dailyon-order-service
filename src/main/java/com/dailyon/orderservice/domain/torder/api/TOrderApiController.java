4ab48756d3c062ff3a7e57ac2e5b50453a3022c3
JunYong Sun <ssjy4974@naver.com>
2023-12-25 23:28:19 +0900

    [ADD] data.sql 수정 및 redirect-url 추가

--- src/main/java/com/dailyon/orderservice/domain/torder/api/TOrderApiController.java
+++ src/main/java/com/dailyon/orderservice/domain/torder/api/TOrderApiController.java
@@ -4,17 +4,23 @@ import com.dailyon.orderservice.domain.torder.api.request.TOrderRequest;
 import com.dailyon.orderservice.domain.torder.api.request.TOrderRequest.TOrderCreateRequest;
 import com.dailyon.orderservice.domain.torder.facade.TOrderFacade;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/orders")
 public class TOrderApiController {
   private final TOrderFacade tOrderFacade;
+
+  @Value("${redirect_url}")
+  private String REDIRECT_URL;
 
   @PostMapping("")
   public ResponseEntity<String> orderReady(
@@ -25,9 +31,10 @@ public class TOrderApiController {
   }
 
   @GetMapping("/approve/{orderId}")
-  public ResponseEntity<String> approve(
-      @PathVariable(name = "orderId") String orderId,
-      @Valid TOrderRequest.OrderApproveRequest request) {
-    return ResponseEntity.ok(tOrderFacade.orderApprove(request.toFacadeRequest(orderId)));
+  public void approve(
+          @PathVariable(name = "orderId") String orderId,
+          @Valid TOrderRequest.OrderApproveRequest request, HttpServletResponse response) throws IOException {
+    String orderNo = tOrderFacade.orderApprove(request.toFacadeRequest(orderId));
+    response.sendRedirect(REDIRECT_URL+"/"+orderNo);
   }
 }
