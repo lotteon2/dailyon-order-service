6a099e339160a088a4523726bdf882a084cc6510
JunYong Sun <ssjy4974@naver.com>
2023-12-26 15:52:16 +0900

    [ADD] MapStruct 추가

---
+++ src/main/java/com/dailyon/orderservice/domain/torder/api/TOrderDtoMapper.java
@@ -0,0 +1,11 @@
+package com.dailyon.orderservice.domain.torder.api;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+    componentModel = "spring",
+    injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+    unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface TOrderDtoMapper {}
