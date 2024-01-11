b3a7fd719d91c763767f121ef187c0373b62c23b
CessnaJ <eon8748@gmail.com>
2024-01-11 15:28:25 +0900

    [FEAT] orderSqsProducer 작성

---
+++ src/main/java/com/dailyon/orderservice/domain/order/sqs/OrderSqsProducer.java
@@ -0,0 +1,34 @@
+package com.dailyon.orderservice.domain.order.sqs;
+
+import com.dailyon.orderservice.domain.order.sqs.dto.SQSNotificationDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderSqsProducer {
+    private final QueueMessagingTemplate sqsTemplate;
+    public static final String GIFT_RECEIVED_NOTIFICATION_QUEUE = "gift-received-notification-queue";
+    public static final String ORDER_COMPLETE_NOTIFICATION_QUEUE = "order-complete-notification-queue";
+
+    public static final String ORDER_SHIPPED_NOTIFICATION_QUEUE = "order-shipped-notification-queue";
+
+    public static final String ORDER_CANCELED_NOTIFICATION_QUEUE = "order-canceled_notification_queue";
+
+
+    public void produce(String queueName, SQSNotificationDto sqsNotificationDto) {
+        // 알림 생성 중 에러 때문에 전체 로직이 취소되는것을 막음.
+        try {
+            Message<String> message = MessageBuilder.withPayload(sqsNotificationDto.toString()).build();
+            sqsTemplate.send(queueName, message);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+}
+
