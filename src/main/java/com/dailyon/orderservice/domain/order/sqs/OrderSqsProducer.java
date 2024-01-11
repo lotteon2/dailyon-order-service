a0c82a52a5e67606d5a6824a9bd6abbceb5bb502
CessnaJ <eon8748@gmail.com>
2024-01-11 16:47:53 +0900

    [FIX] sps메세지 송신시 objectMapper.writeValueAsString

--- src/main/java/com/dailyon/orderservice/domain/order/sqs/OrderSqsProducer.java
+++ src/main/java/com/dailyon/orderservice/domain/order/sqs/OrderSqsProducer.java
@@ -1,6 +1,7 @@
 package com.dailyon.orderservice.domain.order.sqs;
 
 import com.dailyon.orderservice.domain.order.sqs.dto.SQSNotificationDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
@@ -8,11 +9,13 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrderSqsProducer {
     private final QueueMessagingTemplate sqsTemplate;
+    private final ObjectMapper objectMapper;
     public static final String GIFT_RECEIVED_NOTIFICATION_QUEUE = "gift-received-notification-queue";
     public static final String ORDER_COMPLETE_NOTIFICATION_QUEUE = "order-complete-notification-queue";
 
@@ -24,7 +27,8 @@ public class OrderSqsProducer {
     public void produce(String queueName, SQSNotificationDto sqsNotificationDto) {
         // 알림 생성 중 에러 때문에 전체 로직이 취소되는것을 막음.
         try {
-            Message<String> message = MessageBuilder.withPayload(sqsNotificationDto.toString()).build();
+            String jsonMessage = objectMapper.writeValueAsString(sqsNotificationDto);
+            Message<String> message = MessageBuilder.withPayload(jsonMessage).build();
             sqsTemplate.send(queueName, message);
         } catch (Exception e) {
             log.error(e.getMessage());
