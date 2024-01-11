4a5d732457dfa318518d269404823b5334a86a16
CessnaJ <eon8748@gmail.com>
2024-01-11 15:29:19 +0900

    [ADD] 알림 관련 dto, enum 작성

---
+++ src/main/java/com/dailyon/orderservice/domain/order/sqs/dto/RawNotificationData.java
@@ -0,0 +1,59 @@
+package com.dailyon.orderservice.domain.order.sqs.dto;
+
+import com.dailyon.orderservice.domain.order.entity.Order;
+import com.dailyon.orderservice.domain.order.sqs.dto.enums.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RawNotificationData {
+    private String message;
+    private Map<String, String> parameters;
+    private NotificationType notificationType; // 알림 유형
+
+    public static RawNotificationData forGiftReceived(String nickname) {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("nickname", nickname);
+
+        return new RawNotificationData(
+                null,
+                parameters,
+                NotificationType.GIFT_RECEIVED
+        );
+    }
+
+    public static RawNotificationData forOrderComplete(Long orderId, Long totalAmount) {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("orderId", String.valueOf(orderId));
+        parameters.put("totalAmount", String.valueOf(totalAmount));
+
+        return new RawNotificationData(
+                null,
+                parameters,
+                NotificationType.ORDER_COMPLETE
+        );
+    }
+
+    public static RawNotificationData forOrderCanceled(Integer cancelAmount, String productName, Integer productQuantity) {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("cancelAmount", cancelAmount.toString());
+        parameters.put("productName", productName);
+        parameters.put("productQuantity", productQuantity.toString());
+
+        return new RawNotificationData(
+                null,
+                parameters,
+                NotificationType.ORDER_CANCELED
+        );
+    }
+
+
+}
---
+++ src/main/java/com/dailyon/orderservice/domain/order/sqs/dto/SQSNotificationDto.java
@@ -0,0 +1,38 @@
+package com.dailyon.orderservice.domain.order.sqs.dto;
+
+
+import lombok.*;
+
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SQSNotificationDto {
+    List<Long> whoToNotify; // if null, 전체유저에게 발송
+
+    RawNotificationData rawNotificationData;
+
+    public static SQSNotificationDto create(RawNotificationData rawNotificationData) {
+        return SQSNotificationDto.builder()
+                .rawNotificationData(rawNotificationData)
+                .build();
+    }
+
+    public static SQSNotificationDto of(Long whoToNotifty, RawNotificationData rawNotificationData) {
+        return new SQSNotificationDto(
+                Collections.singletonList(whoToNotifty),
+                rawNotificationData
+        );
+    }
+
+    public static SQSNotificationDto of(List<Long> whoToNotifty, RawNotificationData rawNotificationData) {
+        return new SQSNotificationDto(
+                whoToNotifty,
+                rawNotificationData
+        );
+    }
+}
